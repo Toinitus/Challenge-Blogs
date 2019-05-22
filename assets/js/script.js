13b36e86b5c9aa8755537135245e897b6c3a222f
Toinitus <antoinego96@orange.fr>
2019-05-22 20:30:20 +0200

    status final

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,17 @@ if (window.matchMedia("(min-width: 768px)").matches) {
    return Math.round(Math.random() * (max - min + 1)) + min;
  }
   
-  $(window).mousemove(function(e) {
+  $(window).mousemove(function(e) { //Des que le curseur bouge, sa position est enregistré dans les variables (x : horiz; y:vert)
     mousePos.x = e.pageX;
     mousePos.y = e.pageY;
   });
   
-  $(window).mouseleave(function(e) {
+  $(window).mouseleave(function(e) { //Des que le curseur sort de la fenetre, sa position est mise renitialise 
     mousePos.x = -1;
     mousePos.y = -1;
   });
   
-  var draw = setInterval(function(){
+  var draw = setInterval(function(){ //Repetition toutes les 0.3 sec (1 sec = 1000)
     if(mousePos.x > 0 && mousePos.y > 0){
       
       var range = 25;
@@ -35,18 +35,22 @@ if (window.matchMedia("(min-width: 768px)").matches) {
       var sizeInt = getRandomInt(10, 20);
       size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
       
-      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      var leftRandom = getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range);
+      var left = "left: " + leftRandom + "px;";
       
       var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
 
       var style = left+top+color+size;
-      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+
+      // console.log(leftRandom);
+      // console.log(window.innerWidth * 0.98);
+
+      if (leftRandom < (window.innerWidth * 0.97)) {
+  	      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+      }
     }
   }, 300);
 });
-}else{
-
 }
-
 
 
